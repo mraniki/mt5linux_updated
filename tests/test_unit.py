be8b86e9fdd9747e4d560292ac5aea9e07988764
mraniki <8766259+mraniki@users.noreply.github.com>
2024-10-29 11:06:20 +0100

    ✅ Unit Test test: update test_unit.py

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -2,11 +2,19 @@
 mt5linux Unit Testing
 """
 
+import pytest
+
 from mt5linux_updated import MetaTrader5
 
-mt5 = MetaTrader5(port=1235)
-mt5.initialize()
-print(mt5.terminal_info())
-mt5.shutdown()
-assert True
 
+@pytest.fixture
+def mt5():
+    mt5 = MetaTrader5(port=1235)
+    mt5.initialize()
+    yield mt5
+    mt5.shutdown()
+
+
+def test_terminal_info(mt5):
+    info = mt5.terminal_info()
+    assert info is not None
